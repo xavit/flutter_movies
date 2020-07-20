d27b6823bfe3025adf6d7ed6b1e2f69e4657e31a
Javier Claros <javiclaros@gmail.com>
2020-07-20 11:49:10 -0400

    Agregue librería de Swiper y un ejemplo

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hola Mundo!!!'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swiperTarjetas()],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 10.0,
+      ),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
+      ),
     );
   }
 }
